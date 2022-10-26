f42b79eaea55bc428ac93972886a30008c40af2c
Guilherme Manika <guilherme@zorked.net>
2022-10-26 22:10:36 +0200

    Add Gemini protocol support
    It works pretty much like HTTP.
    
    The specification is here https://gemini.circumlunar.space/docs/specification.gmi -  it doesn't go into a lot of detail but it mentions query, fragment and refers to RFC 3986.

--- src/URIs.jl
+++ src/URIs.jl
@@ -304,10 +304,10 @@ function queryparampairs(q::AbstractString)
 end
 
 # Validate known URI formats
-const uses_authority = ["https", "http", "ws", "wss", "hdfs", "ftp", "gopher", "nntp", "telnet", "imap", "wais", "file", "mms", "shttp", "snews", "prospero", "rtsp", "rtspu", "rsync", "svn", "svn+ssh", "sftp" ,"nfs", "git", "git+ssh", "ldap", "s3", "ssh"]
+const uses_authority = ["https", "http", "ws", "wss", "hdfs", "ftp", "gopher", "nntp", "telnet", "imap", "wais", "file", "mms", "shttp", "snews", "prospero", "rtsp", "rtspu", "rsync", "svn", "svn+ssh", "sftp" ,"nfs", "git", "git+ssh", "ldap", "s3", "ssh", "gemini"]
 const non_hierarchical = ["gopher", "hdl", "mailto", "news", "telnet", "wais", "imap", "snews", "sip", "sips"]
-const uses_query = ["http", "wais", "imap", "https", "shttp", "mms", "gopher", "rtsp", "rtspu", "sip", "sips", "ldap"]
-const uses_fragment = ["hdfs", "ftp", "hdl", "http", "gopher", "news", "nntp", "wais", "https", "shttp", "snews", "file", "prospero"]
+const uses_query = ["http", "wais", "imap", "https", "shttp", "mms", "gopher", "rtsp", "rtspu", "sip", "sips", "ldap", "gemini"]
+const uses_fragment = ["hdfs", "ftp", "hdl", "http", "gopher", "news", "nntp", "wais", "https", "shttp", "snews", "file", "prospero", "gemini"]
 
 "checks if a `URI` is valid"
 function Base.isvalid(uri::URI)
