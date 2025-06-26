e00e62d95fb01706e2aedc421bbbc1e57a635166
Dilum Aluthge <dilum@aluthge.com>
2025-06-25 20:55:35 -0400

    Try something

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,7 @@
 function contains_path_traversal(url)
     # Patterns:
     # ../, ..\, /.., \.., ./, .\, /./, \.\
-    PATH_TRAVERSAL = r"(?:\.{2,}[\/\\]|\.{1,}[\/\\]|[\/\\]\.{2,}|[\/\\]\.{1,}[\/\\])"
+    PATH_TRAVERSAL = r"(?:^\.{2,}|\.{2,}$|\.{2,}[\/\\]|\.{1,}[\/\\]|[\/\\]\.{2,}|[\/\\]\.{1,}[\/\\])"
     if occursin(PATH_TRAVERSAL, url)
         return true
     end
